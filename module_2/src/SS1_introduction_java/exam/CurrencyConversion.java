4bf6e58e470315fef654856221ad9bf9de0ae111
nvhoang291299 <nvhoang020100@gmail.com>
2023-02-17 11:39:41 +0700

    suabaitap

--- module_2/src/SS1_introduction_java/exam/CurrencyConversion.java
+++ module_2/src/SS1_introduction_java/exam/CurrencyConversion.java
@@ -8,11 +8,25 @@ public class CurrencyConversion {
     public static void main(String[] args) {
         System.out.println("Số tiền mà bạn muốn chuyển: ");
         double usd = new Scanner(System.in).nextDouble();
-        if (usd <= 0) {
-            System.out.println("giá trị bạn vừa nhập chưa khả dụng");
-        } else {
-            double convers = usd * 23000;
-            System.out.println("Giá trị VND: " + convers + " đồng");
+        while (usd > 0 || usd <= 0) {
+            if (usd <= 0){
+                System.out.println("giá trị bạn vừa nhập chưa khả dụng");
+                System.out.println("bạn hãy nhập lại giá trị");
+                usd = new Scanner(System.in).nextDouble();
+            }
+            else if (usd > 0) {
+                double convers = usd * 23000;
+                System.out.println("Giá trị VND: " + convers + " đồng");
+                break;
+            }
         }
+        // while(temp > 100) {
+        // alert("yêu cầu gaimr nhiệt độ!")
+        // temp = +prompt("nhập nhiệt độ:");
+        // }
+        // while(temp < 20) {
+        // alert("yêu cầu tăng nhiệt độ!")
+        // temp = +prompt("nhập nhiệt độ:");
+        // }
     }
 }
